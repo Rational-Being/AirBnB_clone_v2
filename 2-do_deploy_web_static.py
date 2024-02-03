22a5c53c32a164966f5ed7775972106dba3aebd4
Rational-Being <overcomer021@gmail.com>
2024-02-03 11:47:05 +0100

    2-do_deploy_web_static.py

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -4,7 +4,7 @@ this script distributes an archive to my web servers
 """
 
 from datetime import datetime
-from fabric.api import local, put, run, env
+from fabric.api import local, put, run, env, sudo
 from os.path import exists
 
 env.user = "ubuntu"
@@ -15,19 +15,19 @@ def do_deploy(archive_path):
     """
     this funtion performs te funtion in the comment above
     """
-    if not exists(archive_path):
+    if not (exists(archive_path)):
         return False
     try:
         compres = archive_path.split("/")[-1]
         new_path = "/data/web_static/releases/" + compres.strip(".tgz")
         put(archive_path, "/tmp")
-        run("mkdir -p{}/".format(new_path))
-        run("tar -xzf /tmp/{} -C {}".format(compres, new_path))
-        run("rm /tmp/{}".format(compres))
-        run("mv {}/web_static/* {}".format(new_path, new_path))
-        run("rm -rf {}/web_static".format(new_path))
-        run("rm -rf /data/web_static/current")
-        run("ln -s {} /data/web_static/current".format(new_path))
+        run("sudo mkdir -p{}/".format(new_path))
+        run("sudo tar -xzf /tmp/{} -C {}".format(compres, new_path))
+        run("sudo rm /tmp/{}".format(compres))
+        run("sudo mv {}/web_static/* {}".format(new_path, new_path))
+        run("sudo rm -rf {}/web_static".format(new_path))
+        run("sudo rm -rf /data/web_static/current")
+        run("sudo ln -s {} /data/web_static/current".format(new_path))
         print("New version deployed")
         return True
     except Excception as e:
